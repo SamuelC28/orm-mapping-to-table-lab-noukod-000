1270b1c02731bf2b64b4abe454e005cc7fb9c9b8
Samuel Cornet <samuel@noukod.com>
2018-12-05 18:34:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -23,6 +23,76 @@ class Student
       create row
     end
    end
+   # ..............
+   def self.all_students_in_grade_9
+      sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      SQL
+
+      DB[:conn].execute(sql,"9").map do |row|
+        create row
+      end
+    end
+
+    def self.students_below_12th_grade
+      sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < ?
+      SQL
+
+      DB[:conn].execute(sql,"12").map do |row|
+        create row
+      end
+    end
+
+    def self.first_X_students_in_grade_10 x
+      sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+      SQL
+
+      DB[:conn].execute(sql,10)[0,x]
+    end
+
+    def self.first_student_in_grade_10
+      sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+      ORDER BY id LIMIT 1
+      SQL
+
+      create DB[:conn].execute(sql,10)[0]
+    end
+
+    def self.all_students_in_grade_X x
+      sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+      SQL
+
+      DB[:conn].execute(sql,x).map do |row|
+        create row
+      end
+   end
+   # ....................
+   def self.find_by_name(name)
+     sql = <<-SQL
+     SELECT * FROM students WHERE name = ?
+     SQL
+
+     DB[:conn].execute(sql,name).map do |row|
+       create(row)
+     end.first
+   end
+
+   def save
+       sql = <<-SQL
+         INSERT INTO students (name, grade)
+         VALUES (?, ?)
+       SQL
+
+       DB[:conn].execute(sql, self.name, self.grade)
+
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+   end
 
 
   def self.create_table
